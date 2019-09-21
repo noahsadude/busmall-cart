e34b0fb82d7caae6171ba25e59e236cce65fa5d0
R.James <rjimmy2512@gmail.com>
2019-09-21 13:26:19 -0700

    some constructor functions

--- js/app.js
+++ js/app.js
@@ -1,30 +1,50 @@
 'use strict';
+ var allItems = [];
+ var itemCart = [];
+ var itemCatalogue = [];
+ var cartItemsArr = [];
 
 // Cart constructor.
 var Cart = function(items) {
   // this.items is an array of CartItem instances.
   this.items = items;
+  this.render = function(){
+    var liEl = document.createElement('li');
+    liEl.textContent = this.item;
+    catlist.appendChild(liEl);
+  },
+  allItems.push(this);
 };
+console.log(allItems); 
 
 Cart.prototype.addItem = function(product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
-};
+  var newItem = new Item(e.target.items.value);
+  itemCart.reset();
+  newItem.render();
+}
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  e.preventDefault();
+  
+  localStorage.setItem('cats', JSON.stringify(allItems));
+  console.log('this is what is in local storage', localStorage.cats);
 };
 
 Cart.prototype.removeItem = function(item) {
-  // TODO: Fill in this instance method to remove one item from the cart.
+    // TODO: Fill in this instance method to remove one item from the cart.
   // Note: You will have to decide what kind of parameter to pass in here!
+
 };
+
 
 var CartItem = function(product, quantity) {
   this.product = product;
   this.quantity = quantity;
 };
 
-// Product contructor.
+// Product constructor.
 var Product = function(filePath, name) {
   this.filePath = filePath;
   this.name = name;
